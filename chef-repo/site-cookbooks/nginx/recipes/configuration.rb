ac52c7cc7b296d147a709154f246c2fd0e0be2d8
mhaya <masaharu.hayashi3@gmail.com>
2014-08-12 17:06:36 +0900

    update

--- chef-repo/site-cookbooks/nginx/recipes/configuration.rb
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-#default設定ファイル
-template "#{node['nginx']['dir']}/sites-available/default" do
- source "default.erb" 
- variables({
- })
- notifies :restart, "service[nginx]"
-end
--- chef-repo/site-cookbooks/nginx/recipes/default.rb
+++ chef-repo/site-cookbooks/nginx/recipes/default.rb
@@ -19,3 +19,10 @@ group "www-data" do
      append true
 end
 
+#default設定ファイル
+template "#{node['nginx']['dir']}/sites-available/default" do
+ source "default.erb" 
+ variables({
+ })
+ notifies :restart, "service[nginx]"
+end

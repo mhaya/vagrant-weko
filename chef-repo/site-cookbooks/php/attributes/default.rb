a4fd425ba839f1fcdb9e1a13874da7961bd4133f
mhaya <masaharu.hayashi3@gmail.com>
2014-11-01 05:10:11 +0900

    Minor fix

--- chef-repo/site-cookbooks/php/attributes/default.rb
+++ chef-repo/site-cookbooks/php/attributes/default.rb
@@ -1,10 +1,10 @@
 case node['platform']
 when 'ubuntu'
-default['php']['install_packages'] = %w[php5-common php5-cli]
+default['php']['install_packages'] = %w[php5-common php5-cli php5-xml]
 default['php']['apc']['install_packages'] = %w[php5-apcu php5-gd]
 default['php']['mysql']['install_packages'] = %w[php5-mysql]
 when 'centos'
-default['php']['install_packages'] = %w[php-common php-cli]
+default['php']['install_packages'] = %w[php-common php-cli php-xml]
 default['php']['apc']['install_packages'] = %w[php-apcu php-gd]
 default['php']['mysql']['install_packages'] = %w[php-mysql]
 end

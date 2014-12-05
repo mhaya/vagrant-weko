037077b98e185d93f7afb281d00646aa526afc68
mhaya <masaharu.hayashi3@gmail.com>
2014-12-06 07:57:04 +0900

    Fix #4 Add recipe[xlhtml]

---
+++ chef-repo/site-cookbooks/xlhtml/recipes/default.rb
@@ -0,0 +1,33 @@
+#
+# Cookbook Name:: xlhtml
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+node[:xlhtml][:build_pkgs].each do |pkg|
+  package "#{pkg}" do
+    action :install
+  end
+end
+
+remote_file "#{Chef::Config[:file_cache_path]}/#{node[:xlhtml][:file]}" do
+  source node[:xlhtml][:file_url]
+  checksum node[:xlhtml][:file_checksum]
+  action :create
+end
+
+script "install_xlhtml" do
+    interpreter "bash"
+    user "root"
+    cwd "#{Chef::Config[:file_cache_path]}" 
+    code <<-EOH
+    tar -zxf #{node[:xlhtml][:file]}
+    cd xlhtml
+    ./configure --prefix=/usr
+    make
+    make install
+    EOH
+end

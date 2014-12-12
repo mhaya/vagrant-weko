93fe87cfe9ac1d9df31afb50ad9aed6c075cb827
mhaya <masaharu.hayashi3@gmail.com>
2014-12-12 18:39:31 +0900

    Add recipe[oaicatmuseum]

---
+++ chef-repo/site-cookbooks/oaicatmuseum/attributes/default.rb
@@ -0,0 +1,11 @@
+default[:oaicatmuseum][:webapps_dir] = "/usr/share/tomcat6/webapps"
+default[:oaicatmuseum][:file] = "oaicatmuseum_1.1.war"
+default[:oaicatmuseum][:file_url] = "http://www.oclc.org/content/dam/research/activities/oaicatmuseum/oaicatmuseum_1.1.war"
+default[:oaicatmuseum][:file_checksum] = "b2034e117585d79aee35a2c6edcc07c66074b18e9b30edb8fd5df26e4dec15d2"
+#default[:oaicatmuseum][:file2] = "oaicatdb.sql"
+#default[:oaicatmuseum][:file2_url] = "http://www.oclc.org/content/dam/research/activities/oaicatmuseum/oaicatdb.sql"
+#default[:oaicatmuseum][:file2_checksum] = "4da3b9620495d78fab5c8f1c005ad127492f0c2a4d6712c210e25aa395949fd1"
+default[:oaicatmuseum][:file2] = "oaicatdb2.sql"
+
+
+
---
+++ chef-repo/site-cookbooks/oaicatmuseum/recipes/default.rb
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+#
+# Cookbook Name:: oaicatmuseum
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+remote_file "#{node[:oaicatmuseum][:webapps_dir]}/#{node[:oaicatmuseum][:file]}" do
+  source node[:oaicatmuseum][:file_url]
+  checksum node[:oaicatmuseum][:file_checksum]
+  action :create
+end
+
+# http://www.oclc.org/content/dam/research/activities/oaicatmuseum/oaicatdb.sql
+# を使う場合は以下のコメントアウトを外す。
+# こっから。
+#remote_file "#{Chef::Config[:file_cache_path]}/#{node[:oaicatmuseum][:file2]}" do
+#  source node[:oaicatmuseum][:file2_url]
+#  checksum node[:oaicatmuseum][:file2_checksum]
+#  action :create
+#end
+# ここまで。
+
+# 上記のコメントアウトを外した場合は以下をコメントアウトする。
+# こっから
+template "#{Chef::Config[:file_cache_path]}/#{node[:oaicatmuseum][:file2]}" do
+  source "#{node[:oaicatmuseum][:file2]}.erb"
+  variables({
+    })
+  action :create
+end
+# ここまで
+
+execute "create_oaicatmuseum_db" do
+  command "#{node['site']['mysqladmin_cmd']} -u root create oaicatdb"  
+  not_if "#{node['site']['mysql_cmd']} -u root -D oaicatdb"
+#  notifies :run, 'execute[create_oaicatmuseum_table]', :immediately
+end
+
+execute "create_oaicatmuseum_table" do
+  command "#{node['site']['mysql_cmd']} -u root oaicatdb < #{Chef::Config[:file_cache_path]}/#{node[:oaicatmuseum][:file2]}"
+#  action :nothing
+end
+
+template "#{Chef::Config[:file_cache_path]}/create_oaicatmuseum_user.sql" do
+  source 'create_oaicatmuseum_user.sql.erb'
+  variables({
+    })
+#  action :create
+  notifies :run, 'execute[create_oaicatmuseum_dbuser]', :immediately
+end
+
+execute "create_oaicatmuseum_dbuser" do
+  command "#{node['site']['mysql_cmd']} -u root < #{Chef::Config[:file_cache_path]}/create_oaicatmuseum_user.sql;sleep 300"
+#  action :nothing
+end
+
+script "copy_mysql-connector-java" do
+   interpreter "bash"
+   user "root"
+#   cwd "#{Chef::Config[:file_cache_path]}" 
+   code <<-EOH
+     cp /usr/share/java/mysql-connector-java.jar #{node[:oaicatmuseum][:webapps_dir]}/oaicatmuseum_1.1/WEB-INF/lib/
+   EOH
+#   action :nothing
+end
+
+template "/usr/share/tomcat6/webapps/oaicatmuseum_1.1/WEB-INF/classes/oaicat.properties" do
+ source "oaicat.properties.erb"
+ variables({
+ })
+# mode 0644
+# owner "root"
+# group "root"
+ action :create
+ #notifies :run, "script[copy_mysql-connector-java]", :immediately
+ notifies :restart, "service[tomcat]"
+end
+

955423d35a00aa378b9577201395a81559299b87
mhaya <masaharu.hayashi3@gmail.com>
2015-06-30 09:32:27 +0900

    update weko 2.1.1 > 2.2.0

--- chef-repo/site-cookbooks/php/attributes/default.rb
+++ chef-repo/site-cookbooks/php/attributes/default.rb
@@ -1,14 +1,14 @@
 case node['platform']
 when 'ubuntu'
 default['php']['package']="php5"
-default['php']['install_packages'] = %w[php5-common php5-cli php5-xml php-mbstring]
+default['php']['install_packages'] = %w[php5-common php5-cli php5-xml php5-mbstring]
 default['php']['apc']['install_packages'] = %w[php5-apcu php5-gd]
 default['php']['mysql']['install_packages'] = %w[php5-mysql]
 default['php']['php.ini']="/etc/php.ini"
 default['php']['php.ini_tmpl']="php.ini.centos.erb"
 when 'centos'
 default['php']['package']="php"
-default['php']['install_packages'] = %w[php-common php-cli php-xml]
+default['php']['install_packages'] = %w[php-common php-cli php-xml php-mbstring] 
 default['php']['apc']['install_packages'] = %w[php-apcu php-gd]
 default['php']['mysql']['install_packages'] = %w[php-mysql]
 default['php']['php.ini']="/etc/php.ini"
